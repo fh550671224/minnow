ebdeaa6885d3697665ac96ab9db6b960126e091b
hfu11 <61071935+hfu11@users.noreply.github.com>
2024-03-09 13:48:02 -0800

    buggix

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -57,10 +57,11 @@ void Reassembler::insert_m( ReassembleItem& item )
     it = m.erase( it );
   }
 
-  cout<<item.start<<";"<<item.end<<";"<<item.data<<endl;
 
   // merging finished, insert
   m[item.start] = item;
+
+  cout<<item.start<<";"<<item.end<<";"<<item.data<<";"<<m.size()<<endl;
 }
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
